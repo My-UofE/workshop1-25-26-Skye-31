5c107965538a6dc9b39fe6a5d31e4aca5be5617c
Skye <46286597+Skye-31@users.noreply.github.com>
2026-01-20 10:39:12 +0000

    Finish workshop 1

--- game_functions.py
+++ game_functions.py
@@ -2,13 +2,33 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    x = random.choice(poss_values)   
+    n = len(poss_values)
+    x = poss_values[n // 2]
     return x
 
 # function to be used in game_2: Higher or Lower
-def check_higher_lower(current_val, next_val, user_input):
-    pass
+def check_higher_lower(current_val: int, next_val: int, user_input: str):
+    lower = user_input.lower()
+    if lower == 'h':
+        return next_val > current_val
+    elif lower == 'l':
+        return next_val < current_val
+    else:
+        return False
+
 
 # function to be used in game_3: Hangman
-def process_guess(letter, board, word):
+def process_guess(letter: str, board: list[str], word: str):
+    letter = letter.lower()
+    if letter in word:
+        for i, char in enumerate(word):
+            if letter == char:
+                board[i] = letter
+        print("Well done! '" + letter + "' is in the word")
+        return True
+
+    else:
+        print("Sorry, '" + letter + "' is not in the word")
+        return False
+
     pass
